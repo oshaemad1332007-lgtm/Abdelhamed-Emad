f164ea365f18bb906fb35a6d17566e240d1f7de9
عبدالحميد عماد معروف ابراهيم <oshaemad1332007@gmail.com>
2025-10-24 01:11:52 +0300

    Create Q4.6 sheet3 .c

---
+++ Q4.6 sheet3 .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("enter num1 and num2 : \n");
+    scanf("%d%d", &num1, &num2);
+
+        num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+    printf("num1 = %d\n", num1);
+    printf("num2 = %d", num2);
+    return 0;
+}
