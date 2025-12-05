88f3282a216af185fd8cd95491d952a0a7d798f1
عبدالحميد عماد معروف ابراهيم <oshaemad1332007@gmail.com>
2025-12-05 20:34:48 +0200

    Create Q1.17 sheet5.c

---
+++ Q1.17 sheet5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n = 90;
+    for (int i = 65; i < n; i++)
+    {
+        printf("ASCII value: %d, Character:   %c  \n", i, i);
+    }
+
+    return 0;
+}
