f6d0d337247bbccdcb98db24426f383074ae75e8
عبدالحميد عماد معروف ابراهيم <oshaemad1332007@gmail.com>
2025-12-31 12:08:39 +0200

    Add C program to calculate sum of squares

---
+++ Q 1.9 sheet 6 .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdio.h>
+void sum(int a,int b){
+
+printf("Sum of squares of  %d and %d  is  %d \n",a,b,((a*a)+(b*b)));
+
+}
+
+int main() {
+int a,b;
+scanf("%d%d",&a,&b);
+sum(a,b);
+
+    return 0;
+}
